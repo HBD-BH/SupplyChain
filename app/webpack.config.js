138d45a21cb95fe8280ea7424a238f776d86f908
Bernhard Häfner <bernhard.haefner@tum.de>
2022-04-16 19:11:44 +0200

    refactor: Upload structure for web application frontend (copy from StarNotary Project)

---
+++ app/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: "./src/index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }]),
+  ],
+  devServer: { contentBase: path.join(__dirname, "dist"), compress: true },
+  devtool: 'eval-cheap-source-map'
+};
