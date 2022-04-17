8e728798356a3f003f4ce8d1c4ea8ce7fd51c09f
Bernhard Häfner <bernhard.haefner@tum.de>
2022-04-17 20:40:44 +0200

    feat: Complete frontend and JS code for contract interaction

--- app/src/index.js
+++ app/src/index.js
@@ -2,95 +2,215 @@ import Web3 from "web3";
 import supplyChainArtifact from "../../build/contracts/SupplyChain.json";
 
 const App = {
-  web3: null,
-  account: null,
-  farmer: null,
-  company: null,
-  distributor: null,
-  retailer: null,
-  customer: null,
-  meta: null,
+    web3: null,
+    account: null,
+    farmer: null,
+    company: null,
+    distributor: null,
+    retailer: null,
+    customer: null,
+    meta: null,
 
-  start: async function() {
-    const { web3 } = this;
+    start: async function() {
+        const { web3 } = this;
 
-    try {
-      // get contract instance
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = supplyChainArtifact.networks[networkId];
-      this.meta = new web3.eth.Contract(
-        supplyChainArtifact.abi,
-        deployedNetwork.address,
-        {from: "0x264Ec1Dc6FaBa352A779B4D7e8fee2368e717533"}
-      );
+        try {
+            // get contract instance
+            const networkId = await web3.eth.net.getId();
+            const deployedNetwork = supplyChainArtifact.networks[networkId];
+            this.meta = new web3.eth.Contract(
+                supplyChainArtifact.abi,
+                deployedNetwork.address,
+                {from: "0x264Ec1Dc6FaBa352A779B4D7e8fee2368e717533"}
+            );
 
-      // get accounts
-      const accounts = await web3.eth.requestAccounts();
-      //web3.eth.requestAccounts().then(console.log).catch(console.log);
-      this.account = accounts[0];
-      this.farmer = accounts[0];
-      this.company = accounts[0];
-      this.distributor = accounts[0];
-      this.retailer = accounts[0];
-      this.customer = accounts[0];
-      App.setStatus("Loaded");
+            // get accounts
+            const accounts = await web3.eth.requestAccounts();
+            //web3.eth.requestAccounts().then(console.log).catch(console.log);
+            this.account = accounts[0];
+            this.farmer = accounts[0];
+            this.company = accounts[0];
+            this.distributor = accounts[0];
+            this.retailer = accounts[0];
+            this.customer = accounts[0];
 
-    } catch (error) {
-      console.error(`Could not connect to contract or chain. Error: ${error.message}`);
-    }
-  },
+        } catch (error) {
+            console.error(`Could not connect to contract or chain. Error: ${error.message}`);
+        }
+    },
 
-  setStatus: function(message) {
-    const status = document.getElementById("status");
-    status.innerHTML = message;
-  },
+    setStatus: function(message) {
+        const status = document.getElementById("status");
+        status.innerHTML = message;
+    },
 
-  addRole: async function() {
-    const { addRole } = this.meta.methods;
-    const role = document.getElementById("role").value;
-    await addRole(role, this.farmer).call();
-    App.setStatus("Added role " + role + " to address " + this.farmer + ".");
-  },
+    addRole: async function() {
+        const { addRole } = this.meta.methods;
+        const role = document.getElementById("role").value;
+        await addRole(role, this.farmer).call();
+        App.setStatus("Added role " + role + " to address " + this.farmer + ".");
+    },
 
-  plantSoy: async function() {
-    const { plantSoy } = this.meta.methods;
-    const soyName = document.getElementById("soyName").value;
-    console.log(`Name: ${soyName}`);
-    const soyUpc = document.getElementById("soyUpc").value;
-    console.log(`UPC: ${soyUpc}`);
-    const soyLat = document.getElementById("soyLat").value;
-    console.log(`Lat: ${soyLat}`);
-    const soyLong = document.getElementById("soyLong").value;
-    console.log(`Long: ${soyLong}`);
-    const soyPrice = document.getElementById("soyPrice").value;
-    console.log(`Price: ${soyPrice}`);
-    App.setStatus("Collecting done")
-    await plantSoy(soyName, soyUpc, soyPrice, soyLat, soyLong).send();
-    App.setStatus("Soy with UPC " + soyUpc + "planted.");
-  },
+    plantSoy: async function() {
+        const { plantSoy } = this.meta.methods;
+        const soyName = document.getElementById("soyName").value;
+        const soyUpc = document.getElementById("soyUpc").value;
+        const soyLat = document.getElementById("soyLat").value;
+        const soyLong = document.getElementById("soyLong").value;
+        const soyPrice = document.getElementById("soyPrice").value;
+        await plantSoy(soyName, soyUpc, soyPrice, soyLat, soyLong).send();
+        App.setStatus("Soy with UPC " + soyUpc + "planted.");
+    },
 
-  getSoy: async function() {
-    const { getSoy } = this.meta.methods;
-    const checkUpc = document.getElementById("checkUpc").value;
-    const soyInfo = await getSoy(checkUpc).call();
-    document.getElementById("lblCheckUpcSoy").innerHTML = checkUpc;
-    document.getElementById("lblNameSoy").innerHTML = soyInfo[0];
-    App.setStatus("Soy validated.");
-  },
+    checkSoy: async function() {
+        const { checkSoy } = this.meta.methods;
+        const soyUpc = document.getElementById("soyUpc").value;
+        await checkSoy(soyUpc).send();
+        App.setStatus("Checked soy with UPC " + soyUpc + ".");
+    },
+
+    harvestSoy: async function() {
+        const { harvestSoy } = this.meta.methods;
+        const soyUpc = document.getElementById("soyUpc").value;
+        await harvestSoy(soyUpc).send();
+        App.setStatus("Harvested soy with UPC " + soyUpc + ".");
+    },
+
+    orderSoy: async function() {
+        const { orderSoy } = this.meta.methods;
+        const soyUpc = document.getElementById("soyUpc").value;
+        const balance = document.getElementById("soyOrderEther").value;
+        await orderSoy(soyUpc).send({value: balance});
+        App.setStatus("Address ordered soy with UPC " + soyUpc + ".");
+    },
+
+    shipSoy: async function() {
+        const { shipSoy } = this.meta.methods;
+        const soyUpc = document.getElementById("soyUpc").value;
+        await shipSoy(soyUpc).send();
+        App.setStatus("Soy with UPC " + soyUpc + " shipped.");
+    },
+
+    fetchSoy: async function() {
+        const { fetchSoy } = this.meta.methods;
+        const soyUpc = document.getElementById("soyUpc").value;
+        await fetchSoy(soyUpc).send();
+        App.setStatus("Soy with UPC " + soyUpc + " fetched.");
+    },
+
+    deliverSoy: async function() {
+        const { deliverSoy } = this.meta.methods;
+        const soyUpc = document.getElementById("soyUpc").value;
+        await deliverSoy(soyUpc).send();
+        App.setStatus("Soy with UPC " + soyUpc + " delivered.");
+    },
+
+    makeTofu: async function() {
+        const { makeTofu } = this.meta.methods;
+        const tofuName = document.getElementById("tofuName").value;
+        const tofuUpc = document.getElementById("tofuUpc").value;
+        const tofuLat = document.getElementById("tofuLat").value;
+        const tofuLong = document.getElementById("tofuLong").value;
+        const tofuPrice = document.getElementById("tofuPrice").value;
+        const soyUpc = document.getElementById("tofuSoyUpc").value;
+        await makeTofu(tofuName, soyUpc, tofuUpc, tofuPrice, tofuLat, tofuLong).send();
+        App.setStatus("Made tofu with UPC " + tofuUpc + " from soy with UPC " + soyUpc + ".");
+    },
+
+    orderTofu: async function() {
+        const { orderTofu } = this.meta.methods;
+        const tofuUpc = document.getElementById("tofuUpc").value;
+        const balance = document.getElementById("tofuOrderEther").value;
+        await orderTofu(tofuUpc).send({value: balance});
+        App.setStatus("Address ordered tofu with UPC " + tofuUpc + ".");
+    },
+
+    shipTofu: async function() {
+        const { shipTofu } = this.meta.methods;
+        const tofuUpc = document.getElementById("tofuUpc").value;
+        await shipTofu(tofuUpc).send();
+        App.setStatus("Tofu with UPC " + tofuUpc + " shipped.");
+    },
+
+    fetchTofu: async function() {
+        const { fetchTofu } = this.meta.methods;
+        const tofuUpc = document.getElementById("tofuUpc").value;
+        await fetchTofu(tofuUpc).send();
+        App.setStatus("Tofu with UPC " + tofuUpc + " fetched.");
+    },
+
+    deliverTofu: async function() {
+        const { deliverTofu } = this.meta.methods;
+        const tofuUpc = document.getElementById("tofuUpc").value;
+        await deliverTofu(tofuUpc).send();
+        App.setStatus("Tofu with UPC " + tofuUpc + " delivered.");
+    },
+
+    putTofuOnSale: async function() {
+        const { putTofuOnSale } = this.meta.methods;
+        const tofuUpc = document.getElementById("tofuUpc").value;
+        const tofuRetailPrice = document.getElementById("tofuRetailPrice").value;
+        await putTofuOnSale(tofuUpc, tofuRetailPrice).send();
+        App.setStatus("Tofu with UPC " + tofuUpc + " put on sale.");
+    },
+
+    buyTofu: async function() {
+        const { buyTofu } = this.meta.methods;
+        const tofuUpc = document.getElementById("tofuUpc").value;
+        const balance = document.getElementById("tofuPurchaseEther").value;
+        await buyTofu(tofuUpc).send({value: balance});
+        App.setStatus("Address " + " purchased tofu with UPC " + tofuUpc + ".");
+    },
+
+
+    getSoy: async function() {
+        const { getSoy } = this.meta.methods;
+        const checkUpc = document.getElementById("checkUpcSoy").value;
+        const soyInfo = await getSoy(checkUpc).call();
+        document.getElementById("lblCheckUpcSoy").innerHTML = checkUpc;
+        document.getElementById("lblNameSoy").innerHTML = soyInfo[0];
+        document.getElementById("lblPriceSoy").innerHTML = soyInfo[2];
+        document.getElementById("lblStateSoy").innerHTML = soyInfo[3];
+        document.getElementById("lblFarmer").innerHTML = soyInfo[4];
+        document.getElementById("lblLatSoy").innerHTML = soyInfo[5];
+        document.getElementById("lblLongSoy").innerHTML = soyInfo[6];
+        document.getElementById("lblDistributorSoy").innerHTML = soyInfo[7];
+        document.getElementById("lblBuyer").innerHTML = soyInfo[8];
+        document.getElementById("lblToTofuUpc").innerHTML = soyInfo[9];
+        App.setStatus("Soy validated.");
+    },
+
+    getTofu: async function() {
+        const { getTofu } = this.meta.methods;
+        const checkUpc = document.getElementById("checkUpcTofu").value;
+        const tofuInfo = await getTofu(checkUpc).call();
+        document.getElementById("lblCheckUpcTofu").innerHTML = checkUpc;
+        document.getElementById("lblNameTofu").innerHTML = tofuInfo[0];
+        document.getElementById("lblPriceTofu").innerHTML = tofuInfo[2];
+        document.getElementById("lblStateTofu").innerHTML = tofuInfo[3];
+        document.getElementById("lblProducer").innerHTML = tofuInfo[4];
+        document.getElementById("lblLatTofu").innerHTML = tofuInfo[5];
+        document.getElementById("lblLongTofu").innerHTML = tofuInfo[6];
+        document.getElementById("lblDistributorTofu").innerHTML = tofuInfo[7];
+        document.getElementById("lblRetailer").innerHTML = tofuInfo[8];
+        document.getElementById("lblCustomer").innerHTML = tofuInfo[9];
+        document.getElementById("lblFromSoyUpc").innerHTML = tofuInfo[10];
+        App.setStatus("Tofu validated.");
+    },
 };
 
 window.App = App;
 
 window.addEventListener("load", async function() {
-  if (window.ethereum) {
-    // use MetaMask's provider
-    App.web3 = new Web3(window.ethereum);
-    await window.ethereum.enable(); // get permission to access accounts
-  } else {
-    console.warn("No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live",);
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"),);
-  }
+    if (window.ethereum) {
+        // use MetaMask's provider
+        App.web3 = new Web3(window.ethereum);
+        await window.ethereum.enable(); // get permission to access accounts
+    } else {
+        console.warn("No web3 detected. Falling back to http://127.0.0.1:8545. You should remove this fallback when you deploy live",);
+        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+        App.web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"),);
+    }
 
-  App.start();
+    App.start();
 });
